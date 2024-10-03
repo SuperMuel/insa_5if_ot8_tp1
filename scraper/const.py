4572b84fbc6b85dca4e59fd54b9cf613d01ea0a0
Charles S <cfstcyr.git@pm.me>
2024-10-03 12:38:10 +0200

    add tags to remove

--- scraper/const.py
+++ scraper/const.py
@@ -1,5 +1,5 @@
 USER_AGENT = "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0"
-HTML_TAG_IGNORE = ["script", "style", "img", "figure"]
+HTML_TAG_IGNORE = ["script", "style", "img", "figure", "svg", "a", "button", "link"]
 HTML_ATTR_IGNORE = ["style"]
 
 MD_FORMAT = "# {title}\n\n[URL]({url}) ([Fetched URL]({used_url}))\n\n{content}\n\n"
